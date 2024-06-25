7120811335a0ce288db908079ded7d7c19eb6171
= <=>
2024-06-25 15:17:16 +0100

    Adding Telescope

--- init.lua
+++ init.lua
@@ -5,12 +5,23 @@ vim.call('plug#begin')
 
 Plug('loctvl842/monokai-pro.nvim')
 Plug('itchyny/lightline.vim')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-tree/nvim-web-devicons')
 
 vim.call('plug#end')
 
 require("monokai-pro").setup({
   transparent_background = true,
   terminal_colors = true,
+})
+
+require("telescope").setup({
+  -- add config here  
+})
+
+require('lspconfig').volar.setup({
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
 })
 
 vim.cmd([[colorscheme monokai-pro]])
