50608b27fc0b07d33a05192bebc95fbcd27351d3
Kai Macmaster <kai@macmaster.co.uk>
2024-07-10 09:51:23 +0100

    Added treesitter and Telescope

--- init.lua
+++ init.lua
@@ -5,6 +5,9 @@ vim.call('plug#begin')
 
 Plug('loctvl842/monokai-pro.nvim')
 Plug('itchyny/lightline.vim')
+Plug('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
+Plug('nvim-lua/plenary.nvim')  -- Add this line to include plenary
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' }) 
 
 vim.call('plug#end')
 
@@ -13,7 +16,22 @@ require("monokai-pro").setup({
   terminal_colors = true,
 })
 
+require("telescope")
+
+require("nvim-treesitter.configs").setup({
+  highlight = {
+    enable = true,
+  }
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 vim.cmd([[colorscheme monokai-pro]])
 vim.cmd([[set number]])
 vim.cmd([[let g:lightline = {'colorscheme': 'monokaipro'}]])
+
 
